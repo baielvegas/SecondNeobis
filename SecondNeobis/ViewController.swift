cf7b212bf10b31c19d82c4fcf1eb5acd8bb3117f
baielvegas <30592102+baielvegas@users.noreply.github.com>
2018-10-22 18:37:22 +0600

    Initial Commit

---
+++ SecondNeobis/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondNeobis
+//
+//  Created by Isaev Baiel on 22.10.2018.
+//  Copyright © 2018 Neobis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

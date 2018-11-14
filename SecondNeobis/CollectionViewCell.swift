76fe0714c510e752d7503fb58a875d0b53f2f509
baielvegas <30592102+baielvegas@users.noreply.github.com>
2018-11-14 19:02:26 +0600

    first commit

---
+++ SecondNeobis/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  SecondNeobis
+//
+//  Created by Isaev Baiel on 12.11.2018.
+//  Copyright © 2018 Neobis. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
